bb11daea95db89d8f727571b08c83d103bfaa770
genta wibowo <tetrahedral@gmail.com>
2020-06-20 23:16:01 +0700

    add multiple browser

--- wdio.conf.js
+++ wdio.conf.js
@@ -48,20 +48,25 @@ exports.config = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://docs.saucelabs.com/reference/platforms-configurator
     //
-    capabilities: [{
-        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-        // grid with only 5 firefox instances available you can make sure that not more than
-        // 5 instances get started at a time.
-        maxInstances: 5,
-        //
-        browserName: 'chrome',
-        // browserName: 'firefox',
+    capabilities: [
+        {
+            // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+            // grid with only 5 firefox instances available you can make sure that not more than
+            // 5 instances get started at a time.
+            maxInstances: 5,
+            //
+            browserName: 'chrome',
 
-        // If outputDir is provided WebdriverIO can capture driver session logs
-        // it is possible to configure which logTypes to include/exclude.
-        // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-        // excludeDriverLogs: ['bugreport', 'server'],
-    }],
+            // If outputDir is provided WebdriverIO can capture driver session logs
+            // it is possible to configure which logTypes to include/exclude.
+            // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
+            // excludeDriverLogs: ['bugreport', 'server'],
+            },
+        {
+            maxInstances: 5,
+            browserName: 'firefox',
+        }
+    ],
     //
     // ===================
     // Test Configurations
@@ -109,8 +114,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    // services: ['geckodriver'],
-    services: ['chromedriver'],
+    services: ['chromedriver','geckodriver'],
 
     // OPTIONAL: Provide custom port for geckodriver.
     // geckoDriverRandomPort must be set to false to use this port and maxInstances must be set to 1.
