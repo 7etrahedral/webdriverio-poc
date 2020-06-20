af95debb86d5bbf8a8f1554c6d6da50fe0172461
genta wibowo <tetrahedral@gmail.com>
2020-06-20 22:45:32 +0700

    add firefox capabilities

--- wdio.conf.js
+++ wdio.conf.js
@@ -55,6 +55,8 @@ exports.config = {
         maxInstances: 5,
         //
         browserName: 'chrome',
+        // browserName: 'firefox',
+
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
@@ -107,7 +109,13 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
+    // services: ['geckodriver'],
     services: ['chromedriver'],
+
+    // OPTIONAL: Provide custom port for geckodriver.
+    // geckoDriverRandomPort must be set to false to use this port and maxInstances must be set to 1.
+    // Default: 4444
+    port: 4444,
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
